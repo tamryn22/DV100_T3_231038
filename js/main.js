6a2c130c6b21129948f93440ef6fb83b022bb564
tamryn22 <125568338+tamryn22@users.noreply.github.com>
2023-08-08 15:43:50 +0200

    progress_mark_one
    trips page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+const plants = [
+
+    {"name": "French Riviera",
+    "price": 550,
+    "description": "The Good Life: Imagine indulgent luncheons with chilled rosé overlooking the sea, driving into the hills of medieval Éze, whiling away the afternoon at Monte Carlo’s Princess Grace Rose Garden and strolling Cannes’ palm-lined La Croisette. This is life on the French Riviera.",
+    "image": "cruise1.jpeg"
+    },
+  {
+    "name": "Mediterranean",
+    "price": 400,
+    "description": "Old World Explorations: From ancient ruins and atmospheric old quarters to thriving food scenes and sun-drenched beaches, our Mediterranean cruises offer the perfect combination of old-world beauty and modern charisma. ",
+    "image": "cruise2.jpeg"
+  },
+  {
+    "name": "Greek Isles",
+    "price": 950,
+    "description": "Made For The Gods: Our Greek Isles cruises open the door to journeys that unfold in lands of epic history and play out on sultry islands lined with whitewashed villages.",
+    "image": "cruise3.jpeg"
+  },
+  {
+    "name": "Greenland",
+    "price": 850,
+    "description": "Majestic Landscapes: Our Greenland & Iceland cruises bring you to the edge of the Arctic Circle and fill your days with magnificent fjords, dramatic natural wonders and outdoor adventures on these remote North Atlantic island countries. ",
+    "image": "cruise4.jpeg"
+  },
+  {
+    "name": "Alaska",
+    "price": 1200,
+    "description": "The Last Frontier: Experience Alaska the way it was meant to be with Oceania Cruises. Revel in the natural wonders of The Last Frontier while enjoying a luxurious small ship experience aboard the beautifully re-inspired Regatta. Our Alaska cruises immerse you in the best of the 49th state – calving glaciers, breathtaking fjords, fascinating wildlife encounters and lively historic gold mining towns like Juneau and Skagway. ",
+    "image": "cruise5.jpeg"
+  }
+    
+];
+
 $(document).ready(function ()  {
 
     $("#checkout").click(function(){
@@ -12,4 +46,63 @@ $(document).ready(function ()  {
         .slideDown(1000)
     });
     
+
+    
+    
+    
+    $(document).ready(function(){
+    //select the plants container and add html from our template 
+    $("#removeone").click(function (){
+      $("#one").hide();
+    });
+    
+    $("#removetwo").click(function (){
+      $("#two").hide();
+    });
+    
+    $("#removethree").click(function (){
+      $("#three").hide();
+    });
+    
+    //you have to call it only when ready/loaded
+        loadPlants();
+    });
+    
+    
+    function loadPlants () {
+        for (let i = 0; i < plants.length; i++) {
+            console.log(plants[i]);
+    
+            $("#plantsContainer").append($("#plantCardTemplate").html());
+    
+            let currentChild = $("#plantsContainer").children().eq(i+1);
+    
+            $(currentChild).find(".card-img-top").attr('src','assets/' + plants[i].image);
+    
+            $(currentChild).find("#nameText").text(plants[i].name);
+    
+            $(currentChild).find("#priceText").text('R ' + plants[i].price);
+    
+            $(currentChild).find("#descriptionText").text(plants[i].description);
+    
+            $(currentChild).find("#descriptionText").hide();
+            
+            
+        }
+    }
+    
+    
+    $("#plantsContainer").on('click', '.card', function(){
+    
+      // Toggle the price & description text
+      $(this).find("#priceText").toggle();
+      $(this).find("#descriptionText").toggle();
+    
+      // Resize the image to fit the additional content
+      $(this).find(".card-img-top").toggleClass("small");
+    
+      
+    });
+    
 });
+
